74b51bc24c029002e03c2a89ed8e17a9a69a4c92
Alex Grover <hello@alexgrover.me>
2024-01-28 13:52:06 -0500

    Add basic header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { PlusIcon } from 'lucide-react'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/cn'
+
 export default function Home() {
-  return <main />
+  return (
+    <>
+      <header className="container flex h-14 max-w-screen-2xl items-center justify-between border-b">
+        <Link href="/" className="font-bold">
+          fardrop
+        </Link>
+        <Link
+          href="/new"
+          className={cn(
+            buttonVariants({ className: 'flex items-center gap-2' }),
+          )}
+        >
+          <PlusIcon className="h-4 w-4" />
+          <span>Create</span>
+        </Link>
+      </header>
+      <main />
+    </>
+  )
 }
